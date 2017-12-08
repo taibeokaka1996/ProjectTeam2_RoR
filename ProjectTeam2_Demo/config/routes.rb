76549b36f5589e8bcf3d95571be17655197077c7
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-12-08 10:17:05 +0700

    Change template Login and Sign up

--- ProjectTeam2_Demo/config/routes.rb
+++ ProjectTeam2_Demo/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :users
+  resources :users do
+    get '/SignUp', to: 'users#new'
+    get '/Login', to: 'users#index'    
+  end
   root 'home#index', as: "home_index"
 
   get '/about', to: 'home#about'
-  get '/SignUp', to: 'users#new'
-  get '/Login', to: "user#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
